577aa366aaf38adf708dd4c6b3e57492739c8185
v0id <gavrosh100120102nd@gmail.com>
2026-08-29 04:40:12 +0300

    New: Added basic CLI help and version commands.

--- cmd/yosi/main.go
+++ cmd/yosi/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+ const version = "0.1.0"
 
 func main() {
-	fmt.Println("YOSI package resolver-builder v1")
+	if len(os.Args) < 2 {
+		printHelp()
+		return 
+	}
+
+
+ command := os.Args[1]
+
+switch command {
+
+case "help":
+	printHelp()
+
+case "version":
+	printVersion()
+
+default:
+	fmt.Printf("Unknown command: %s\n", command)
+	printHelp()
+}
+}
+
+func printHelp() {
+	fmt.Println(`YOSI - source-based package manager
+
+Usage:
+  yosi <command>
+
+Commands:
+  help       Show this help message
+  version    Show YOSI version
+`)
+}
+
+func printVersion() {
+	fmt.Printf("YOSI %s\n", version)
 }
