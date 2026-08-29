6ce8c91724fbef40192442f8308de2344784aa98
v0id <gavrosh100120102nd@gmail.com>
2026-08-29 06:35:07 +0300

    New: Added HTTP reposetory client

--- internal/repo/repo.go
+++ internal/repo/repo.go
@@ -1,5 +1,11 @@
 package repo
 
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
 type Package struct {
 	Name    string `json:"name"`
 	Version string `json:"version"`
@@ -9,4 +15,23 @@ type Index struct {
 	Repository string    `json:"repository"`
 	Version    string    `json:"version"`
 	Packages   []Package `json:"packages"`
+}
+
+const indexURL = "https://repo.yosi-repo.ru/v1/index.json"
+
+var httpClient = &http.Client{
+	Timeout: 10 * time.Second,
+}
+
+func FetchIndex() (Index, error) {
+	resp, err := httpClient.Get(indexURL)
+	if err != nil {
+		return Index{}, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return Index{}, fmt.Errorf("repository returned HTTP status %d", resp.StatusCode)
+	}
+	return Index{}, nil
 }
